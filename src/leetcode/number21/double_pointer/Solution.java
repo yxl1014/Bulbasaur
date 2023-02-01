621ee4d432db66bb404aa3b690bcd73c4fc073e8
yxl <1627419257@qq.com>
2023-02-02 01:33:48 +0800

    leetcode #21

---
+++ src/leetcode/number21/double_pointer/Solution.java
@@ -0,0 +1,80 @@
+package leetcode.number21.double_pointer;
+
+/**
+ * @author yxl
+ * @date 2023/2/2 上午1:17
+ */
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 时间 0 ms 击败 100%
+     * 内存 40.8 MB 击败 73.83%
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = null;
+        ListNode temp = null;
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                if (res == null) {
+                    res = list2;
+                    list2=list2.next;
+                    temp = res;
+                    continue;
+                }
+                temp.next = list2;
+                list2 = list2.next;
+                temp=temp.next;
+                continue;
+            }
+            if (list2 == null) {
+                if (res == null) {
+                    res = list1;
+                    list1=list1.next;
+                    temp = res;
+                    continue;
+                }
+                temp.next = list1;
+                list1 = list1.next;
+                temp=temp.next;
+                continue;
+            }
+            if (list1.val <= list2.val) {
+                if (res == null) {
+                    res = list1;
+                    list1=list1.next;
+                    temp = res;
+                    continue;
+                }
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                if (res == null) {
+                    res = list2;
+                    list2=list2.next;
+                    temp = res;
+                    continue;
+                }
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp=temp.next;
+        }
+        return res;
+    }
+}
---
+++ src/leetcode/number21/recursion/Solution.java
@@ -0,0 +1,48 @@
+package leetcode.number21.recursion;
+
+/**
+ * @author yxl
+ * @date 2023/2/2 上午1:30
+ */
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 时间 0 ms 击败 100%
+     * 内存 41.1 MB 击败 44.78%
+     * 作者：腐烂的橘子
+     * 链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/103891/yi-kan-jiu-hui-yi-xie-jiu-fei-xiang-jie-di-gui-by-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+
